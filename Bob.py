f66f0e4eebacb0954e68b83e23936ecf4574ec1e
alan-leonn <alan.leon12@yahoo.com>
2023-04-26 09:45:15 -0500

    DH for server/client

--- Bob.py
+++ Bob.py
@@ -1,13 +1,32 @@
 # echo-client.py
 
+import math
 import socket
+import sys
+
+import pyDHE
+
+# print("input a key:")
+# msg = input()
+# str_msg = str(msg)
+# new_msg = bytes(msg, encoding='utf8')
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+    # s.sendall(new_msg)
+    # data = s.recv(1024)
+    bob = pyDHE.new(14)
+    bob_pub_key = bob.getPublicKey()
+    bob_pub_key_bytes = bob_pub_key.to_bytes(math.ceil(bob_pub_key.bit_length()/8), sys.byteorder, signed=False)
+    s.sendall(bob_pub_key_bytes)
+    alice_pub_key_bytes = s.recv(2048)  # may need to change to 1024
+    alice_pub_key = int.from_bytes(alice_pub_key_bytes, sys.byteorder, signed=False)
+    shared_key = bob.update(alice_pub_key)
+    small_key = int(str(shared_key)[:128])
+    print(small_key)
 
-print(f"Received {data!r}")
+
+# print(f"Received {data!r}")
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import math
 import socket
+import sys
 
+import pyDHE
 
-#print("this works")
+# print("this works")
+
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -9,14 +13,20 @@ PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+
+    # while True: (this causes following code to be unreachable)
     conn, addr = s.accept()
+
     with conn:
         print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
 
-
-
+        alice = pyDHE.new(14)
+        bob_pub_key_bytes = conn.recv(2048)
+        bob_pub_key = int.from_bytes(bob_pub_key_bytes, sys.byteorder, signed=False)
+        shared_key = alice.update(bob_pub_key)
+        alice_pub_key = alice.getPublicKey()
+        alice_pub_key_bytes = alice_pub_key.to_bytes(math.ceil(alice_pub_key.bit_length()/8), sys.byteorder,
+                                                     signed=False)
+        conn.sendall(alice_pub_key_bytes)
+        
+        print(shared_key)
