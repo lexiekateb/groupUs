f1637496c1dc355ce0ca24ba27c3bba04cd1e5a7
alan-leonn <alan.leon12@yahoo.com>
2023-04-28 21:08:21 -0500

    DH for client/server and stays active until client disconnects

--- Bob.py
+++ Bob.py
@@ -1,32 +1,44 @@
-# echo-client.py
-
-import math
 import socket
-import sys
-
 import pyDHE
+import sys
+import math
 
-# print("input a key:")
-# msg = input()
-# str_msg = str(msg)
-# new_msg = bytes(msg, encoding='utf8')
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    # s.sendall(new_msg)
-    # data = s.recv(1024)
-    bob = pyDHE.new(14)
-    bob_pub_key = bob.getPublicKey()
-    bob_pub_key_bytes = bob_pub_key.to_bytes(math.ceil(bob_pub_key.bit_length()/8), sys.byteorder, signed=False)
-    s.sendall(bob_pub_key_bytes)
-    alice_pub_key_bytes = s.recv(2048)  # may need to change to 1024
-    alice_pub_key = int.from_bytes(alice_pub_key_bytes, sys.byteorder, signed=False)
-    shared_key = bob.update(alice_pub_key)
-    small_key = int(str(shared_key)[:128])
-    print(small_key)
+# Diffie-Hellman function for key generation
+bob = pyDHE.new(14)
+bob_pub_key = bob.getPublicKey()
+bob_pub_key_bytes = bob_pub_key.to_bytes(math.ceil(bob_pub_key.bit_length() / 8), sys.byteorder, signed=False)
+client.sendall(bob_pub_key_bytes)
+alice_pub_key_bytes = client.recv(2048)  # may need to change to 1024
+alice_pub_key = int.from_bytes(alice_pub_key_bytes, sys.byteorder, signed=False)
+shared_key = bob.update(alice_pub_key)
+small_key = int(str(shared_key)[:128])
+# print(small_key)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
 
 
-# print(f"Received {data!r}")
+send(" ")
+text = input()
+while text:
+    send(text)
+    text = input()
+
+send(DISCONNECT_MESSAGE)
+
--- main.py
+++ main.py
@@ -1,32 +1,59 @@
-import math
 import socket
+import threading
+import math
 import sys
-
 import pyDHE
 
-# print("this works")
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+    # Perform Diffie-Hellman key exchange
+    alice = pyDHE.new(14)
+    bob_pub_key_bytes = conn.recv(2048)
+    bob_pub_key = int.from_bytes(bob_pub_key_bytes, sys.byteorder, signed=False)
+    shared_key = alice.update(bob_pub_key)
+    alice_pub_key = alice.getPublicKey()
+    alice_pub_key_bytes = alice_pub_key.to_bytes(math.ceil(alice_pub_key.bit_length() / 8), sys.byteorder, signed=False)
+    conn.sendall(alice_pub_key_bytes)
+    small_key = int(str(shared_key)[:128])
+    conn.send(f"Here is your key: {small_key}".encode(FORMAT))
 
-    # while True: (this causes following code to be unreachable)
-    conn, addr = s.accept()
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
 
-    with conn:
-        print(f"Connected by {addr}")
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
 
-        alice = pyDHE.new(14)
-        bob_pub_key_bytes = conn.recv(2048)
-        bob_pub_key = int.from_bytes(bob_pub_key_bytes, sys.byteorder, signed=False)
-        shared_key = alice.update(bob_pub_key)
-        alice_pub_key = alice.getPublicKey()
-        alice_pub_key_bytes = alice_pub_key.to_bytes(math.ceil(alice_pub_key.bit_length()/8), sys.byteorder,
-                                                     signed=False)
-        conn.sendall(alice_pub_key_bytes)
-        
-        print(shared_key)
+    # disconnect the client when they leave the server
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
