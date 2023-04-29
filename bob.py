1e1fad18441e7edc53475183a2fda975646bd956
alan-leonn <alan.leon12@yahoo.com>
2023-04-28 23:01:50 -0500

    made small edit to bob file

--- bob.py
+++ bob.py
@@ -1,6 +1,10 @@
 import socket
 import tkinter as tk
+from functools import partial
 from tkinter import *
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
 
 # Setting up the window
 bob = tk.Tk()
